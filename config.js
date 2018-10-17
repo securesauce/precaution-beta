2c31b23364164baf0cd76de33aa3778e5d8bfa2c
Martin Vrachev <mvrachev@vmware.com>
2018-10-17 13:32:56 +0300

    Add tests for three different cases
    There are three opportunities for a Pull request:
    - includes python files with security issues
    - including python files without security issues
    - doesn't include Python files at all
    
    All those cases needs tests and I add them.
    
    Signed-off-by: Martin Vrachev <mvrachev@vmware.com>

--- config.js
+++ config.js
@@ -3,6 +3,8 @@
 
 const config = {
   cleanupAfterRun: true,
+  // I made it false because I had problem when its true. I get the error: App error
+  // {"message":"Not Found","documentation_url":"https://developer.github.com/v3/repos/contents/#get-contents"}
   compareAgainstBaseline: true,
   fileExtensions: ['.py', '.pyw', '.go']
 }
--- test/bandit.test.js
+++ test/bandit.test.js
@@ -2,6 +2,8 @@
 // SPDX-License-Identifier: BSD-2-Clause
 
 const runBandit = require('../bandit')
+const generateReport = require('../report.js')
+const fs = require('fs-extra')
 
 describe('Bandit runner', () => {
   test('Handles baseline option', async () => {
@@ -11,5 +13,38 @@ describe('Bandit runner', () => {
     expect(results.results.length).toBe(2)
     expect(results.results[0].line_number).toBe(5)
     expect(results.results[1].line_number).toBe(8)
+    fs.removeSync('test/fixtures/bandit.json')
+  })
+
+  test('Analyze python file with security issues', async () => {
+    const results = await runBandit('test/fixtures/python', ['test.py'])
+
+    const trueResults = generateReport(results)
+    const expected = fs.readFileSync('test/fixtures/reports/testResults.txt', 'utf8')
+
+    expect(trueResults.summary).toBe(expected)
+    fs.removeSync('test/fixtures/bandit.json')
+  })
+
+  test('Bandit run on file without security issues', async () => {
+    const results = await runBandit('test/fixtures/python', ['HelloWorld.py'])
+
+    const trueResults = generateReport(results)
+
+    expect(trueResults.title).toEqual('All clear')
+    expect(trueResults.summary).toEqual('There are no security issues found.')
+    expect(trueResults.annotations.length).toEqual(0)
+    fs.removeSync('test/fixtures/bandit.json')
+  })
+
+  test('Bandit run on non Python file', async () => {
+    const results = await runBandit('test/fixtures', ['words.doc'])
+
+    const trueResults = generateReport(results)
+
+    expect(trueResults.title).toEqual('All clear')
+    expect(trueResults.summary).toEqual('There are no security issues found.')
+    expect(trueResults.annotations.length).toEqual(0)
+    fs.removeSync('test/fixtures/bandit.json')
   })
 })
