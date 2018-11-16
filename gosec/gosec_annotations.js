74b8652a6787fbc940f021d57760eb40b62807a1
Martin Vrachev <martin.vrachev@gmail.com>
2018-11-16 10:21:40 +0200

    Gosec report and report tests (#35)
    I added the gosec/gosec_annotation.js file which generates annotations from issues found by gosec in the way the Github API wants them.
    
    Also I added the gosec_report.js file which is very familiar with the bandit_report.js file but using the gosec json format.
    
    I added two tests in the test/gosec.report.test.js file and that's why I added also the test/fixtures/reports/gosec_mix_results.json file.
    
    Signed-off-by: Martin Vrachev <mvrachev@vmware.com>

---
+++ gosec/gosec_annotations.js
@@ -0,0 +1,29 @@
+// Copyright 2018 VMware, Inc.
+// SPDX-License-Identifier: BSD-2-Clause
+
+const { getAnnotationLevel } = require('../annotations_levels')
+
+/**
+ * @param {*} issue - an issue from which the annotation will be build
+ * @return {Object} returns an annotation object as specified here:
+ * https://developer.github.com/v3/checks/runs/#annotations-object
+ */
+function getAnnotation (issue) {
+  const path = issue.file
+  const startLine = issue.line
+  const endLine = issue.line
+  const annotationLevel = getAnnotationLevel(issue.severity, issue.confidence)
+  const title = `${issue.rule_id}:${issue.details}`
+  const message = `The issue is in the code: ${issue.code}`
+
+  return {
+    path,
+    start_line: startLine,
+    end_line: endLine,
+    annotation_level: annotationLevel,
+    title,
+    message
+  }
+}
+
+module.exports.getAnnotation = getAnnotation
