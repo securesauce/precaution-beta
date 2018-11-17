5a5e5fe3eb45ceea6582bf2c879aa1467d3955e0
Martin Vrachev <mvrachev@vmware.com>
2018-11-17 12:05:33 +0200

    Removed full path and add require on top
    I removed the full path from the test/fixtures/annotations/gosec_mix_annotations.json file and add requires for banditAnnotations and gosecAnnotations on top of the test/merge.reports.test.js file.
    
    Signed-off-by: Martin Vrachev <mvrachev@vmware.com>

--- test/merge.reports.test.js
+++ test/merge.reports.test.js
@@ -3,6 +3,8 @@
 
 const { config } = require('../config')
 const mergeReports = require('../merge_reports')
+const banditAnnotations = require('./fixtures/annotations/mixed_levels_annotations.json').annotations
+const gosecAnnotations = require('./fixtures/annotations/gosec_mix_annotations.json').annotations
 
 describe('Merge reports tests from Bandit and Gosec reports', () => {
   test('No issues found from both Gosec and Bandit', () => {
@@ -20,11 +22,10 @@ describe('Merge reports tests from Bandit and Gosec reports', () => {
   })
 
   test('Issues found by Bandit and no issues found by Gosec', () => {
-    const { annotations } = require('./fixtures/annotations/mixed_levels_annotations.json')
     const { metrics } = require('./fixtures/reports/mix_results.json')
     const banditSummary = JSON.stringify(metrics || 'N/A', null, '\n')
 
-    let banditReport = { title: config.issuesFoundResultTitle, summary: banditSummary, annotations: annotations }
+    let banditReport = { title: config.issuesFoundResultTitle, summary: banditSummary, annotations: banditAnnotations }
     let gosecReport = { title: config.noIssuesResultTitle, summary: config.noIssuesGosecResultSummary, annotations: [] }
 
     let expectedSummary = '\n' + 'Gosec summary: \n' + config.noIssuesGosecResultSummary + '\n \n' +
@@ -34,16 +35,15 @@ describe('Merge reports tests from Bandit and Gosec reports', () => {
     const result = mergeReports(banditReport, gosecReport)
     expect(result.title).toEqual(config.issuesFoundResultTitle)
     expect(result.summary).toEqual(expectedSummary)
-    expect(result.annotations).toEqual(annotations)
+    expect(result.annotations).toEqual(banditAnnotations)
   })
 
   test('No issues found by Bandit but issues are found by Gosec', () => {
-    let { annotations } = require('./fixtures/annotations/gosec_mix_annotations.json')
     let { stats } = require('./fixtures/reports/gosec_mix_results.json')
     const gosecSummary = JSON.stringify(stats || 'N/A', null, '\n')
 
     let banditReport = { title: config.noIssuesResultTitle, summary: config.noIssuesBanditResultSummary, annotations: [] }
-    let gosecReport = { title: config.issuesFoundResultTitle, summary: gosecSummary, annotations: annotations }
+    let gosecReport = { title: config.issuesFoundResultTitle, summary: gosecSummary, annotations: gosecAnnotations }
 
     let expectedSummary = '\n' + 'Gosec summary: \n' + gosecReport.summary + '\n \n' +
           '-----------------------------------------------------------------' +
@@ -52,27 +52,25 @@ describe('Merge reports tests from Bandit and Gosec reports', () => {
     const result = mergeReports(banditReport, gosecReport)
     expect(result.title).toEqual(config.issuesFoundResultTitle)
     expect(result.summary).toEqual(expectedSummary)
-    expect(result.annotations).toEqual(annotations)
+    expect(result.annotations).toEqual(gosecAnnotations)
   })
 
   test('Issues found by Bandit and by Gosec', () => {
-    const { annotations } = require('./fixtures/annotations/mixed_levels_annotations.json')
     const { metrics } = require('./fixtures/reports/mix_results.json')
     const banditSummary = JSON.stringify(metrics || 'N/A', null, '\n')
 
-    let gosecAnn = require('./fixtures/annotations/gosec_mix_annotations.json')
     let { stats } = require('./fixtures/reports/gosec_mix_results.json')
     const gosecSummary = JSON.stringify(stats || 'N/A', null, '\n')
 
-    let banditReport = { title: config.issuesFoundResultTitle, summary: banditSummary, annotations: annotations }
-    let gosecReport = { title: config.issuesFoundResultTitle, summary: gosecSummary, annotations: gosecAnn.annotations }
+    let banditReport = { title: config.issuesFoundResultTitle, summary: banditSummary, annotations: banditAnnotations }
+    let gosecReport = { title: config.issuesFoundResultTitle, summary: gosecSummary, annotations: gosecAnnotations }
 
     let expectedSummary = '\n' + 'Gosec summary: \n' + gosecReport.summary + '\n \n' +
           '-----------------------------------------------------------------' +
           '\nBandit summuary' + '\n' + banditReport.summary
 
     let expectedAnnotations = []
-    expectedAnnotations = expectedAnnotations.concat(gosecAnn.annotations, annotations)
+    expectedAnnotations = expectedAnnotations.concat(gosecAnnotations, banditAnnotations)
 
     const result = mergeReports(banditReport, gosecReport)
     expect(result.title).toEqual(config.issuesFoundResultTitle)
