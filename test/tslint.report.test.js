edf10ec36ef2880a1717ac37f6298e180737bdfd
Martin Vrachev <mvrachev@vmware.com>
2019-03-22 19:58:49 +0200

    Add configuration for TSLint
    I added configuration for TSLint to scan both JavaScript and TypeScript.
    
    Signed-off-by: Martin Vrachev <mvrachev@vmware.com>

---
+++ test/tslint.report.test.js
@@ -0,0 +1,50 @@
+// Copyright 2019 VMware, Inc.
+// SPDX-License-Identifier: BSD-2-Clause
+
+const generateReport = require('../tslint/tslint_report')
+
+const tslintResults = require('./fixtures/reports/tslint_vulnerable.json')
+
+describe('TSLint report generation', () => {
+  let report
+
+  beforeEach(() => {
+    report = generateReport(tslintResults, 'test/fixtures/js-ts')
+  })
+
+  test('Creates correct report structure', () => {
+    expect(report).toHaveProperty('annotations')
+    expect(report).toHaveProperty('issueCount.errors')
+    expect(report).toHaveProperty('issueCount.warnings')
+    expect(report).toHaveProperty('issueCount.notices')
+    expect(report).toHaveProperty('moreInfo')
+  })
+
+  test('Creates correct annotations', () => {
+    expect(report.annotations).toHaveLength(7)
+    expect(report.annotations[0].end_line).toBe(55)
+    expect(report.annotations[3].start_line).toBe(4)
+    expect(report.annotations[5].path).toBe('testFile.js')
+    expect(report.annotations[6].title).toEqual('tsr-detect-non-literal-fs-filename')
+
+    report.annotations.forEach(annotation => {
+      expect(annotation.annotation_level).toMatch(/notice|warning|failure/)
+      expect(annotation.message).not.toBe('')
+    })
+  })
+
+  test('Creates correct issue count', () => {
+    expect(report.issueCount.errors).toBe(3)
+    expect(report.issueCount.warnings).toBe(4)
+    expect(report.issueCount.notices).toBe(0)
+  })
+
+  test('Handles no vulnerable reports', async () => {
+    const report = generateReport([], '')
+
+    expect(report.annotations).toHaveLength(0)
+    expect(report.issueCount.errors).toBe(0)
+    expect(report.issueCount.warnings).toBe(0)
+    expect(report.issueCount.notices).toBe(0)
+  })
+})
