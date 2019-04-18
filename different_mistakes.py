dd6ac3879fb868a8bf889d67322a00287d2b4a58
Martin Vrachev <mvrachev@vmware.com>
2019-04-18 11:24:28 +0300

    [DON'T MERGE] Debbuging
    Signed-off-by: Martin Vrachev <mvrachev@vmware.com>

---
+++ different_mistakes.py
@@ -0,0 +1,27 @@
+import os
+
+def someFunction(user, password="Admin"):
+    print("Hi " + user)
+
+def someFunction2(password):
+    if password == "root":
+        print("OK, logged in")
+
+def noMatch(password):
+    if password == '':
+        print("No password!")
+
+def NoMatch2(password):
+    if password == "ajklawejrkl42348swfgkg":
+        print("Nice password!")
+
+def doLogin(password="blerg"):
+    pass
+
+def NoMatch3(a, b):
+    pass
+
+import os
+
+os.chmod('/etc/passwd', 0o227)
+os.chmod('~/.bashrc', 511)
